29f06ce86231ad813f405586f2b8a44220eaaf8c
yukiycu <w225431d@yokohama-cu.ac.jp>
2025-08-25 15:54:13 +0900

    Initial commit

--- filter/structural_alert_filter.py
+++ filter/structural_alert_filter.py
@@ -3,7 +3,7 @@ import medchem as mc
 from chemtsv2.filter import Filter
 from chemtsv2.utils import transform_linker_to_mol
 
-class StructualAlertFilter(Filter):
+class StructuralAlertFilter(Filter):
     def check(mol, config):
         alerts = mc.structural.CommonAlertsFilters()
         try:
@@ -16,9 +16,9 @@ class StructualAlertFilter(Filter):
             return False
         return results["reasons"][0] == None
     
-class StructualAlertFilterForXMol(Filter):
+class StructuralAlertFilterForXMol(Filter):
     def check(mol, conf):
         @transform_linker_to_mol(conf)
         def _check(mol, conf):
-            return StructualAlertFilter.check(mol, conf)
+            return StructuralAlertFilter.check(mol, conf)
         return _check(mol, conf)
