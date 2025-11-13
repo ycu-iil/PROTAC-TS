3ec3b4681c384ee1d776fd96320906198c0ec1f1
yukiycu <w225431d@yokohama-cu.ac.jp>
2025-11-13 11:01:54 +0900

    update attachment_points_filter.py

--- filter/attachment_points_filter.py
+++ filter/attachment_points_filter.py
@@ -5,5 +5,6 @@ from chemtsv2.filter import Filter
 
 class AttachmentPointsFilter(Filter):
     def check(mol, conf):
+        Chem.SanitizeMol(mol)
         smi = Chem.MolToSmiles(mol)
         return smi.count('*') == conf['attachment_points_filter']['threshold']
